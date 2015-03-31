989233bd167db10ce3f23d54c0b70771c3050c33
Clemens Kaposi <clemens@kaposi.name>
2015-03-31 06:07:51 +0200

    Convert README to reStructuredText

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='WTForms-Polyglot',
-    version='0.1',
+    version='0.1.1',
     url='http://github.com/yggi49/wtforms-polyglot',
     license='BSD',
     author='Clemens Kaposi',
